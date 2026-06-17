a4cd855cb85cbc3120d03de7ce35bff81d3befb1
phucdt1234 <phucdt1234@gmail.com>
2026-06-17 10:14:09 +0700

    dieu chinh driver theo main hien tai

--- backend/src/main/java/com/unibus/api/driver/BusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unibus.api.driver;
-
-import com.unibus.api.driver.model.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus, Integer> {
-}
